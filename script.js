b7a484b9820c788e06af1c1239a7b9af625e584e
Julie Lykke Christensen <julielykkechristensen@Julie-Lykkes-Macbook.local>
2021-01-04 22:02:27 +0100

    en masse

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
-function myFunction(imgs) {
-    // Get the expanded image
-    var expandImg = document.getElementById("expandedImg");
-    // Get the image text
-    var imgText = document.getElementById("imgtext");
-    // Use the same src in the expanded image as the image being clicked on from the grid
-    expandImg.src = imgs.src;
-    // Use the value of the alt attribute of the clickable image as text inside the expanded image
-    imgText.innerHTML = imgs.alt;
-    // Show the container element (hidden with CSS)
-    expandImg.parentElement.style.display = "block";
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+}
+
+function toggleMenu() {
+    console.log("toggleMenu")
+
+    document.querySelector("#menuItems").classList.toggle("hidden");
+}
+
+function burgerMenu(x) {
+    x.classList.toggle("change");
 }
